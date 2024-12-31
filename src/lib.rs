11e5a66672eb8c68ff57086cd524c8e0e1949a22
abus-sh <58699376+abus-sh@users.noreply.github.com>
2024-12-30 21:44:51 -0500

    Improved crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,45 @@
 //! [here](https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-dosdatetimetofiletime),
 //! though the documentation for `DOSTime` and `DOSDate` also includes explantions for the format.
 //! 
-//! By default, this crate treats integers as big-endian and arrays as little-endian. In the future,
-//! support for treating arrays as big-endian will be added.
-
-// TODO: create try_from_le and try_from_be for DOSTime & DOSDate that uses [u8; 2]
+//! ```
+//! use dostime::{DOSDate, DOSTime, DOSDateTime};
+//! use dostime::date::DateError;
+//! use dostime::time::TimeError;
+//! use dostime::datetime::DateTimeError;
+//! 
+//! // Valid dates and times can be constructed.
+//! let date = DOSDate::new(2017, 4, 6).unwrap();           // 2017-04-06
+//! let time = DOSTime::new(13, 24, 54).unwrap();           // 13:24:54
+//! let datetime = DOSDateTime::new(2006, 4, 15, 1, 5, 34); // 2006-04-15 01:05:34
+//! 
+//! // Invalid dates and times can't be constructed and information about what is invalid is
+//! // returned.
+//! let invalid_month = DOSDate::new(1996, 13, 3).unwrap_err();
+//! assert_eq!(invalid_month, DateError::InvalidMonth);
+//! 
+//! let invalid_second = DOSTime::new(1, 2, 60).unwrap_err();
+//! assert_eq!(invalid_second, TimeError::InvalidSecond);
+//! 
+//! let invalid_day = DOSDateTime::new(2001, 4, 31, 6, 13, 12).unwrap_err();
+//! assert_eq!(invalid_day, DateTimeError::DateError(DateError::InvalidDay));
+//! 
+//! // Dates and times can be converted to and from integers and arrays of bytes.
+//! let int: u16 = date.into();
+//! assert_eq!(int, 0x4A86);
+//! assert_eq!(date, DOSDate::try_from(0x4A86).unwrap());
+//! 
+//! let bytes: [u8; 2] = time.into();
+//! assert_eq!(bytes, [0x1B, 0x6B]);
+//! assert_eq!(time, DOSTime::try_from([0x1B, 0x6B]).unwrap());
+//! ```
 
 /// In cases where big-endian vs little-endian differences matter, this module contains traits that
 /// can be used to define `Into`, `TryInto`, `From`, and `TryFrom` equivalents that are specifically
 /// little- or big-endian.
+/// 
+/// As with the normal traits, `FromLE` creates implementations of `TryFromLE` and `IntoLE` and
+/// `IntoLE` creates an implementation of `TryIntoLE`. This also applies for the big-endian trait
+/// variants.
 pub mod traits;
 
 /// This module contains `DOSTime` and its related functionality. It allows for conversion to and
